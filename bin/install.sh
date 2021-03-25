b2de8e025bb19d3d299eb807560592c4df19ca6b
yukishinonome <yuki.yamakoshi0126@gmail.com>
2021-03-26 00:35:55 +0900

    インストールするツール追加

--- bin/install.sh
+++ bin/install.sh
@@ -3,6 +3,10 @@
 mkdir ~/.zsh
 curl -o ~/.zsh/git-completion.zsh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
 curl -o ~/.zsh/git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+
+brew install ghq
+brew install peco
 
 ln -sf ~/.dotfiles/.vimrc ~/.vimrc
 ln -sf ~/.dotfiles/.zshrc ~/.zshrc
